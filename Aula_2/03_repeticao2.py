9b5dde4e6bd3054d956d643db30751bff395eebc
Edson Marciano Rodrigues Padilha <68876@PROWAY.TREINA>
2020-06-13 11:47:55 -0300

    Aula_2 com For

---
+++ Aula_2/03_repeticao2.py
@@ -0,0 +1,16 @@
+numeros = range(5)
+print(numeros)
+
+for i in range(5):
+    print(i)
+
+lista_idades = [10,15,21,48]
+print(lista_idades)
+
+# Percorrendo a lista com for e range
+for i in range(len(lista_idades)):
+    print(lista_idades[i])
+
+# Percorrendo a lista com for. 'foreach'
+for i in lista_idades:
+    print(i)
