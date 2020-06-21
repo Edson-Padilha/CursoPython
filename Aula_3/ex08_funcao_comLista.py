a5fdafbf96eb4f5f00b11666c158af97e5d9520b
Edson-Padilha <edsonp.bsi@gmail.com>
2020-06-20 22:24:10 -0300

    Atualizando print

--- Aula_3/ex08_funcao_comLista.py
+++ Aula_3/ex08_funcao_comLista.py
@@ -5,7 +5,10 @@ def media(lista):
     return resultado
 
 for i in range (4):
+    print('='*30)
     nota = float(input(f'Digite nota {i+1}: '))
     notas.append(nota)
-print(media(notas))
-        
+    
+print('='*30)
+print(f'Sua média é: {media(notas)}')
+print('='*30)
