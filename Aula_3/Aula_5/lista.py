1a8afc17ed7ffbab0b1a4992c3d6efbedebcd42f
Edson Marciano Rodrigues Padilha <68876@PROWAY.TREINA>
2020-07-04 09:17:49 -0300

    aula5

---
+++ Aula_3/Aula_5/lista.py
@@ -0,0 +1,18 @@
+lista = [[], "oi", "thau",1.98,1, [1,2,3],{"agua":123},(), "fim"]
+
+for i in lista:
+
+    if type(i) == str:
+        print('Tipo: 01 - String')
+    elif type(i) == int:
+        print('Tipo: 02 - Inteiro') 
+    elif type(i) == float:
+        print('Tipo: 03 - Real')
+    elif type(i) == list:
+        print('Tipo: 04 - Lista')
+    elif type(i) == tuple:
+        print('Tipo: 05 - Tupla')
+    elif type(i) == dict:
+        print('Tipo: 01 - Dicionário')
+
+print(len(lista))
---
+++ Aula_3/Aula_5/print.py
@@ -0,0 +1,5 @@
+a = 10.66666661
+print('O numero é: ', a)
+print(f'O numero é: {a:.2f}')
+print('O numero é: {:.2f}'.format(a))
+
