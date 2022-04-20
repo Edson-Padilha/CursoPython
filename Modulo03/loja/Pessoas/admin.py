851c498cbd46a04effe3a1d1ddc0e5744b465577
Edson Marciano Rodrigues Padilha <b12206@bra01ad01.local>
2022-04-19 22:12:25 -0300

    Arquivo Django

---
+++ Modulo03/loja/Pessoas/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Cliente, CPFCNPJ
+# Register your models here.
+admin.site.register(CPFCNPJ)
+admin.site.register(Cliente)
---
+++ Modulo03/loja/Pessoas/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class CPFCNPJ(models.Model):
+    numero = models.CharField(max_length=14,unique=True)# Valor não pode se repetir
+    
+    def __str__(self):
+        return self.numero
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100, blank=True)
+    fone = models.CharField(max_length=11)
+    cpf_cnpj = models.OneToOneField(CPFCNPJ, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        db_table = 'Cliente'
+
